5d4c0502a1864a0a2b146033866a5f83eeaee008
wanndrueakk <wanndrueakk@users.noreply.github.com>
2017-12-26 15:04:22 +0300

    sedona index

---
+++ sedona/js/script.js
@@ -0,0 +1,7 @@
+var form = document.querySelector(".main-form");
+var search = document.querySelector(".search-btn");
+
+search.addEventListener("click", function(event) {
+  event.preventDefault();
+  form.classList.toggle("modal-show");
+})
